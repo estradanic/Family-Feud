03a0e7ccf5d401951dcbba2e9d8067290d4ee5c0
Nicholas Estrada <nestrada@openeye.net>
2025-05-30 16:36:41 -0700

    Added more customization options

--- public/js/FamilyFeud.js
+++ public/js/FamilyFeud.js
@@ -8,11 +8,24 @@ var app = {
     currentQ: 0,
     wrong:0,
     board: $(`<div class='gameBoard'>
-
                 <!--- Scores --->
+                <div class='teamSection'>
+                    <div class='teamNameContainer' id='team1Container'>
+                        <input type='text' class='teamNameInput hide' id='team1Input' placeholder='Team 1 Name' maxlength='20' value='Team 1'>
+                        <div class='teamName' id='team1Name'>Team 1</div>
+                    </div>
+                    <div class='score' id='team1'>0</div>
+                </div>
+                
                 <div class='score' id='boardScore'>0</div>
-                <div class='score' id='team1' >0</div>
-                <div class='score' id='team2' >0</div>
+                
+                <div class='teamSection'>
+                    <div class='teamNameContainer' id='team2Container'>
+                        <input type='text' class='teamNameInput hide' id='team2Input' placeholder='Team 2 Name' maxlength='20' value='Team 2'>
+                        <div class='teamName' id='team2Name'>Team 2</div>
+                    </div>
+                    <div class='score' id='team2'>0</div>
+                </div>
 
                 <!--- Main Board --->
                 <div id='middleBoard'>
@@ -42,10 +55,11 @@ var app = {
                     <div id="wrong"       class='button wrongX'>
                         <img alt="not on board" src="/public/img/Wrong.svg"/>
                     </div>
-                    <div id='awardTeam2'  class='button' data-team='2' >Award Team 2</div>
+                    <div id='prevQuestion' class='button'>Previous Question</div>
+                    <div id='resetScores' class='button'>Reset Scores</div>
+                    <div id='awardTeam2'  class='button' data-team='2'>Award Team 2</div>
                 </div>
-
-                </div>`),
+            </div>`),
     
     // Utility functions
     shuffle: (array) => {
@@ -83,7 +97,23 @@ var app = {
         var holderMain = app.board.find(".colHolder");
 
         boardScore.html(0);
-        question.html(qText.replace(/&x22;/gi, '"'));
+        
+        // Clean text and store it as a data attribute for reference
+        var cleanText = qText.replace(/&x22;/gi, '"');
+        question.attr('data-text', cleanText);
+        
+        // Set content based on role
+        if (app.role === "host") {
+            // Host always sees the question
+            question.html(cleanText);
+        } else {
+            // Clients start with question hidden
+            question.html('');
+        }
+        
+        // Track client-side visibility state (separate from what host sees)
+        question.data('client-revealed', false);
+        
         holderMain.empty();
 
         app.wrong = 0;
@@ -139,6 +169,46 @@ var app = {
         });
         cards.data("flipped", false);
     },
+
+    // Question reveal toggle function
+    toggleQuestion: () => {
+        if (app.role === "host") {
+            var question = app.board.find(".question");
+            var isClientRevealed = question.data('client-revealed');
+            
+            // Toggle client-side visibility state
+            var newState = !isClientRevealed;
+            question.data('client-revealed', newState);
+            
+            // Emit socket event to update clients
+            app.socket.emit("talking", {
+                trigger: 'toggleQuestion',
+                revealed: newState
+            });
+        }
+    },
+    
+    // Type animation function
+    typeQuestion: () => {
+        var question = app.board.find(".question");
+        var text = question.attr('data-text');
+        var length = text.length;
+        var current = 0;
+        
+        // Clear any existing content
+        question.html('');
+        
+        // Type out the text character by character
+        var interval = setInterval(function() {
+            if (current < length) {
+                question.html(question.html() + text.charAt(current));
+                current++;
+            } else {
+                clearInterval(interval);
+            }
+        }, 30); // Speed of typing
+    },
+    
     getBoardScore: () => {
         var cards = app.board.find('.card');
         var boardScore = app.board.find('#boardScore');
@@ -193,10 +263,81 @@ var app = {
         app.currentQ++;
         app.makeQuestion(app.currentQ);
     },
+    
+    // New function to navigate to previous question
+    previousQuestion: () => {
+        if (app.currentQ > 0) {
+            app.currentQ--;
+            app.makeQuestion(app.currentQ);
+        }
+    },
+    
+    // New function to reset both team scores to zero
+    resetScores: () => {
+        if (app.role == "host") {
+            // Reset visual scores
+            app.board.find('#team1, #team2').html('0');
+            
+            // Play a sound effect (optional)
+            $("#ding")[0].play();
+            
+            // Notify clients
+            app.socket.emit("talking", {
+                trigger: 'resetScores'
+            });
+        }
+    },
+    
+    // New function to enable direct score editing
+    enableScoreEditing: (teamNumber) => {
+        const teamScore = app.board.find(`#team${teamNumber}`);
+        const currentScore = teamScore.text();
+        
+        // Create an input field with the current score
+        const input = $(`<input type='number' class='scoreEdit' value='${currentScore}' min='0'>`);
+        
+        // Replace the score display with the input
+        teamScore.html(input);
+        input.focus().select();
+        
+        // Handle when input is complete (blur or enter)
+        input.on('blur keypress', function(e) {
+            if (e.type === 'blur' || e.which === 13) {
+                const newScore = parseInt($(this).val()) || 0;
+                
+                // Update locally
+                teamScore.html(newScore);
+                
+                // Broadcast to other clients
+                app.socket.emit("talking", {
+                    trigger: 'updateScore',
+                    team: teamNumber,
+                    score: newScore
+                });
+                
+                e.preventDefault();
+            }
+        });
+    },
+    
     makeHost: () => {
         app.role = "host";
+        
+        // Show the question for the host
+        var question = app.board.find(".question");
+        question.html(question.attr('data-text'));
+        
+        // Make input fields visible and hide the corresponding display elements
+        app.board.find('.teamNameInput').removeClass('hide');
+        
+        // Remove the "Be the host" button
+        app.board.find('#hostBTN').remove();
+        
+        // Show the host controls
         app.board.find(".hide").removeClass('hide');
         app.board.addClass('showHost');
+        
+        // Notify other clients that host is assigned
         app.socket.emit("talking", {
             trigger: 'hostAssigned'
         });
@@ -229,6 +370,39 @@ var app = {
 
     },
 
+    // Update the team name handling functionality
+    // Update the updateTeamName function to properly handle team name display
+    updateTeamName: (teamNumber, newName) => {
+        // Update the team name display
+        app.board.find(`#team${teamNumber}Name`).text(newName);
+        
+        // Update the award button text
+        app.board.find(`#awardTeam${teamNumber}`).text(`Award ${newName}`);
+        
+        // Update the input field value (only if it's not the source)
+        const inputField = app.board.find(`#team${teamNumber}Input`);
+        if (inputField.val() !== newName) {
+            inputField.val(newName);
+        }
+    },
+
+    // Handle team name input events
+    handleTeamNameInput: (teamNumber) => {
+        return function() {
+            const newName = $(this).val();
+            
+            // Update locally (both display and button)
+            app.updateTeamName(teamNumber, newName);
+            
+            // Then broadcast to other clients
+            app.socket.emit("talking", {
+                trigger: 'teamNameChange',
+                team: teamNumber,
+                name: newName
+            });
+        };
+    },
+
     // Socket Test
     talkSocket: (e) => {
         if (app.role == "host") app.socket.emit("talking", e.data);
@@ -239,20 +413,46 @@ var app = {
             case "newQuestion":
                 app.changeQuestion();
                 break;
+            case "prevQuestion":
+                app.previousQuestion();
+                break;
             case "awardTeam1":
                 app.awardPoints(1);
                 break;
             case "awardTeam2":
                 app.awardPoints(2);
                 break;
+            case "resetScores":
+                app.board.find('#team1, #team2').html('0');
+                break;
+            case "updateScore":
+                app.board.find(`#team${data.team}`).html(data.score);
+                break;
             case "flipCard":
                 app.flipCard(data.num);
                 break;
             case "hostAssigned":
+                // When someone becomes host, remove the "Be the host" button from all clients
                 app.board.find('#hostBTN').remove();
                 break;
             case "wrong":
                 app.wrongAnswer()
+                break;
+            case "toggleQuestion":
+                if (app.role !== "host") {
+                    if (data.revealed) {
+                        // Show question with typing animation for clients
+                        app.typeQuestion();
+                    } else {
+                        // Hide question for clients
+                        app.board.find(".question").html('');
+                    }
+                    app.board.find(".question").data('client-revealed', data.revealed);
+                }
+                break;
+            case "teamNameChange":
+                // Update team name on all clients
+                app.updateTeamName(data.team, data.name);
                 break;
         }
     },
@@ -267,6 +467,23 @@ var app = {
         app.board.find('#awardTeam2' ).on('click', { trigger: 'awardTeam2' }, app.talkSocket);
         app.board.find('#newQuestion').on('click', { trigger: 'newQuestion'}, app.talkSocket);
         app.board.find('#wrong'      ).on('click', { trigger: 'wrong'      }, app.talkSocket);
+        app.board.find('#prevQuestion').on('click', { trigger: 'prevQuestion' }, app.talkSocket);
+        app.board.find('#resetScores').on('click', { trigger: 'resetScores' }, app.talkSocket);
+        
+        // Add double-click handlers for score editing (host only)
+        app.board.find('#team1').on('dblclick', function() {
+            if (app.role === "host") app.enableScoreEditing(1);
+        });
+        app.board.find('#team2').on('dblclick', function() {
+            if (app.role === "host") app.enableScoreEditing(2);
+        });
+        
+        // Add event handlers for team name inputs
+        app.board.find('#team1Input').on('input', app.handleTeamNameInput(1));
+        app.board.find('#team2Input').on('input', app.handleTeamNameInput(2));
+        
+        // Add click handler for the question element
+        app.board.find('.questionHolder').on('click', app.toggleQuestion);
 
         app.socket.on('listening', app.listenSocket)
     }
